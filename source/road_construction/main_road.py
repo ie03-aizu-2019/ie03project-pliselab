aa6b21f346ac826943f3becfcca8bc2a0576efd6
ryoya_ito <horusu.dqx@gmail.com>
2019-06-16 20:28:04 +0900

    実装ファイル作成

---
+++ source/road_construction/main_road.py
@@ -0,0 +1,20 @@
+from road_construction import Point, Side
+from . import Side, Point, list_cross_point
+
+def main_road():
+    
+    return
+
+def connected_road(points: [Point], sides: [Side]):
+    """与えられたグラフの連結成分を検出します。
+
+    Args:
+        s (str): 開始地点ID。
+        g (str): 目的地点ID。
+        V (Dict[str, Dict[str, float]]): 重み付きグラフ
+        n (int, optional): いくつの経路を求めるか。 Defaults to 1.
+
+    Returns:
+        Tuple[float, List[str]]: 最短経路の距離と経路。
+    """
+    return
