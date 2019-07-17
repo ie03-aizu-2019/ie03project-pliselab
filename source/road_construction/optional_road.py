7278135109c9c081addd46ac2811c972a4185018
ryoya_ito <horusu.dqx@gmail.com>
2019-07-17 13:46:34 +0900

    不要なメソッド削除

--- source/road_construction/optional_road.py
+++ source/road_construction/optional_road.py
@@ -2,28 +2,7 @@ from typing import List
 from . import Side, Point, list_cross_point
 import math
 
-
-def calc_distance(side: Side, point: Point) -> float:
-    """
-    与えられた点と線分との距離を計算します。
-    https://mathtrain.jp/tentotyokusen
-    これのdの式のままにやる たぶんうまくいってない
-
-    Args:
-        side (Side): 線分
-        point (Point): 座標
-
-    Returns:
-        線分と座標の間の最短距離
-    """
-    a = ((side.side_from.y - side.side_to.y) / (side.side_from.x - side.side_to.x))
-    b = -1
-    c = -a * side.side_to.x + side.side_to.y
-
-    d = abs(a * point.x + b * point.y + c) / math.sqrt(a**2 + b**2)
-    return d
-
-def calc_distance_vec(side: Side, point: Point) -> (float, Point):
+def calc_distance(side: Side, point: Point) -> (float, Point):
     """
     与えられた点と線分との距離を計算します。
     ベクトルでの計算ver
@@ -70,7 +49,7 @@ def suggest_optional_road(sides: List[Side], point: Point) -> Point:
     min_distance = None
     min_side_cross_point = None
     for side in sides:
-        tup = calc_distance_vec(side, point)
+        tup = calc_distance(side, point)
         if min_distance is None or min_distance > abs(tup[0]):
             min_distance = tup[0]
             min_distance_side = side
