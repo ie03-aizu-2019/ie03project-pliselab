23147929d27c35884e8f5a0d615927142b842042
ryoya_ito <horusu.dqx@gmail.com>
2019-07-17 13:31:11 +0900

    実装完了

--- source/road_construction/optional_road.py
+++ source/road_construction/optional_road.py
@@ -23,7 +23,7 @@ def calc_distance(side: Side, point: Point) -> float:
     d = abs(a * point.x + b * point.y + c) / math.sqrt(a**2 + b**2)
     return d
 
-def calc_distance_vec(side: Side, point: Point) -> float:
+def calc_distance_vec(side: Side, point: Point) -> (float, Point):
     """
     与えられた点と線分との距離を計算します。
     ベクトルでの計算ver
@@ -33,25 +33,25 @@ def calc_distance_vec(side: Side, point: Point) -> float:
         point (Point): 座標
 
     Returns:
-        線分と座標の間の最短距離
+        線分と座標の間の最短距離, 最短距離の座標
     """
 
     # ベクトル
     vecA = (side.side_to.x - side.side_from.x, side.side_to.y - side.side_from.y)
-    vecB = (point.x - side.side_from.x, point.y - side.side_from.y)
+    tmp = float(-(vecA[0] * (side.side_from.x - point.x) + vecA[1] * (side.side_from.y - point.y))) / ((vecA[0] ** 2) + (vecA[1] ** 2))
+    cross_point = None
+    distance = 0.0
 
-    # 内積
-    inner = vecA[0] * vecB[0] + vecA[1] * vecB[1]
-    print("side", side)
-    print("point", point)
-    print("vecA", vecA)
-    print("vecB", vecB)
-    print("inner", inner)
-    print()
-
-    # 距離
-    distance = inner / (vecA[0] * vecA[0] + vecA[1] * vecA[1])
-    return distance
+    if tmp < 0:
+        cross_point = side.side_from
+        distance = math.sqrt((cross_point.x - point.x) ** 2 + ((cross_point.y - point.y) ** 2))
+    elif tmp > 1:
+        cross_point = side.side_to
+        distance = math.sqrt((cross_point.x - point.x) ** 2 + ((cross_point.y - point.y) ** 2))
+    else:
+        cross_point = Point(vecA[0] * tmp + side.side_from.x, vecA[1] * tmp + side.side_from.y)
+        distance = math.sqrt((cross_point.x - point.x) ** 2 + ((cross_point.y - point.y) ** 2))
+    return (float(distance), cross_point)
 
 def suggest_optional_road(sides: List[Side], point: Point) -> Point:
     """
@@ -65,27 +65,17 @@ def suggest_optional_road(sides: List[Side], point: Point) -> Point:
         最適な道 に繋いだ時の交差地点の座標
     """
 
-    cross_point: Point
-
     # 線分と座標との距離が一番近いものを取得
     min_distance_side = None
     min_distance = None
+    min_side_cross_point = None
     for side in sides:
-        d = calc_distance_vec(side, point)
-        print(d)
-        if min_distance is None or min_distance > d:
-            min_distance = d
+        tup = calc_distance_vec(side, point)
+        if min_distance is None or min_distance > abs(tup[0]):
+            min_distance = tup[0]
             min_distance_side = side
-    print("最短距離の線分", min_distance_side)
-    if min_distance <= 0.0:
-        cross_point = min_distance_side.side_to
-    elif min_distance >= 1:
-        cross_point = min_distance_side.side_from
-    else:
-        x = min_distance_side.side_from.x + min_distance * (min_distance_side.side_to.x - min_distance_side.side_from.x)
-        y = min_distance_side.side_from.y + min_distance * (min_distance_side.side_to.y - min_distance_side.side_from.y)
-        cross_point = Point(x, y)
-
-    sides += [Side(cross_point, point)]
-    return cross_point
+            min_side_cross_point = tup[1]
+    sides += [Side(min_side_cross_point, point)]
+    
+    return min_side_cross_point
     
