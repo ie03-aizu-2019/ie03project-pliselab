b57f856e19508bca9ee1fbdfc91ba3313dcb8e76
nabekou <nabekou0729@gmail.com>
2019-07-31 04:05:34 +0900

    小課題10の一部実装

--- source/main.py
+++ source/main.py
@@ -4,70 +4,150 @@ from typing import List, Optional
 
 import argparse
 import math
+import matplotlib.pyplot as plt
+import time
 
 from road_construction import Point, Side
 import road_construction as rc
+import graph_generator as gg
 
 # mode
 parser = argparse.ArgumentParser()
 parser.add_argument('-m', '--mode')
 args = parser.parse_args()
-mode = args.mode or '4'
-if mode == '5':
-    mode = '3'
-if mode == '6':
-    mode = '4'
+mode = args.mode or 'interactive'
 
-# input
-input_nums = list(map(int, input().split()))
-
-N, M, P, Q = input_nums
-
-# 端点の列挙
-# points: List[Point] = []
-# for i in range(N):
-#     x, y = map(int, input().split())
-#     points.append(Point(x, y))
-points: List[Point] = [Point(*(map(int, input().split()))) for i in range(N)]
-
-# 辺の列挙
-# sides: List[Side] = []
-# for i in range(M):
-#     fr, to = map(int, input().split())
-#     sides.append(Side(points[fr - 1], points[to - 1]))
-sides: List[Side] = [Side(points[f - 1], points[t - 1])
-                     for f, t in [map(int, input().split()) for i in range(M)]]
-
-# 小課題1
-if mode == '1':
-    is_cross, cross_point = rc.calc_cross_point(sides[0], sides[1])
-    print(cross_point if is_cross else "NA")
-
-# 小課題2
-# 交差地点の列挙
-cross_points = rc.list_cross_point(sides)
-if mode == '2':
-    for point in cross_points:
-        print(point)
+mode_dict = {'5': '3', '6': '4', '10': 'interactive'}
+if mode in mode_dict.keys():
+    mode = mode_dict[mode]
 
 
-# 入力
-additional_points = []
-for i in range(P):
-    additional_points.append(Point(*map(int, input().split())))
+def main():
+    if mode == 'interactive':
+        interactive()
+    else:
+        run()
 
-# 経路の列挙
-V = rc.build_graph(sides, points, cross_points)
-# 小課題3,4,5,6
-for i in range(Q):
-    f_id, t_id, k = input().split()
-    if mode == '3' or mode == '4':
-        for dist, path in rc.decide_k_shortest_path(f_id, t_id, V, int(k)):
-            print(f'{dist:.6g}' if dist != math.inf else "NA")
-            if mode == '4' and dist != math.inf:
-                print(' '.join(path))
 
-# 小課題7
-if mode == '7':
-    for point in additional_points:
-        print(rc.suggest_optional_road(sides, point))
+def run():
+    # input
+    input_nums = list(map(int, input().split()))
+
+    N, M, P, Q = input_nums
+
+    # 端点の列挙
+    # points: List[Point] = []
+    # for i in range(N):
+    #     x, y = map(int, input().split())
+    #     points.append(Point(x, y))
+    points: List[Point] = [Point(*(map(int, input().split())))
+                           for i in range(N)]
+
+    # 辺の列挙
+    # sides: List[Side] = []
+    # for i in range(M):
+    #     fr, to = map(int, input().split())
+    #     sides.append(Side(points[fr - 1], points[to - 1]))
+    sides: List[Side] = [Side(points[f - 1], points[t - 1])
+                         for f, t in [map(int, input().split()) for i in range(M)]]
+
+    # 小課題1
+    if mode == '1':
+        is_cross, cross_point = rc.calc_cross_point(sides[0], sides[1])
+        print(cross_point if is_cross else "NA")
+
+    # 小課題2
+    # 交差地点の列挙
+    cross_points = rc.list_cross_point(sides)
+    if mode == '2':
+        for point in cross_points:
+            print(point)
+
+    # 入力
+    additional_points = []
+    for i in range(P):
+        additional_points.append(Point(*map(int, input().split())))
+
+    # 経路の列挙
+    V = rc.build_graph(sides, points, cross_points)
+    # 小課題3,4,5,6
+    for i in range(Q):
+        f_id, t_id, k = input().split()
+        if mode == '3' or mode == '4':
+            for dist, path in rc.decide_k_shortest_path(f_id, t_id, V, int(k)):
+                print(f'{dist:.6g}' if dist != math.inf else "NA")
+                if mode == '4' and dist != math.inf:
+                    print(' '.join(path))
+
+    # 小課題7
+    if mode == '7':
+        for point in additional_points:
+            print(rc.suggest_optional_road(sides, point))
+
+
+def interactive():
+    points, sides, cross_points = [], [], []
+    plot_flg = False
+    while True:
+        try:
+            query_str = input(">>> ")
+            query = query_str.split(' ')
+            q = query[0]
+            if q == 'add':
+                points.append(Point(int(query[1]), int(query[2])))
+                print(f'add {len(points)}')
+
+            elif q in ['connect', 'con']:
+                fr, to = query[1], query[2]
+                if fr == to:
+                    print(f'Choose different points')
+                    continue
+                fr_point = points[int(
+                    fr) - 1] if fr[0] != 'C' else cross_points[int(fr[1:]) - 1]
+                to_point = points[int(
+                    to) - 1] if to[0] != 'C' else cross_points[int(to[1:]) - 1]
+
+                sides.append(Side(fr_point, to_point))
+                cross_points = rc.list_cross_point(sides)
+                print(f'connect {fr} and {to}')
+
+            elif q == 'list':
+                if query[1] == 'crosspoint':
+                    for point in cross_points:
+                        print(point)
+                elif query[1] == 'bridge':
+                    # TODO 橋(幹線道路)の列挙
+                    pass
+
+            elif q == 'search':
+                f_id, t_id = query[1], query[2]
+                k = int(query[3]) if len(query) > 3 else 1
+                V = rc.build_graph(sides, points, cross_points)
+                for dist, path in rc.decide_k_shortest_path(f_id, t_id, V, k):
+                    print(f'{dist:.6g}' if dist != math.inf else "NA")
+                    if dist != math.inf:
+                        print(' '.join(path))
+
+            elif q == 'plot':
+                if len(query) > 1 and query[1] == 'close':
+                    plt.close()
+                    plot_flg = False
+                else:
+                    plot_flg = True
+            elif q in ['quit', 'q', 'exit']:
+                plt.close()
+                break
+            else:
+                print(f'Illegal Query: {query_str}')
+
+            if plot_flg:
+                plt.close()
+                gg.create_graph(points, cross_points, sides, [],
+                                max([p.x for p in points] + [p.y for p in points] + [0]))
+                plt.show(block=False)
+        except Exception as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    main()
